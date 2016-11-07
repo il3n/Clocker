432ea7cf14062407f03be12252e9a66953243ab6
jarvis <jarvis.lee.8906@gmail.com>
2016-11-07 09:55:49 +0800

    init

---
+++ Clocker/ClockerView.h
@@ -0,0 +1,13 @@
+//
+//  ClockerView.h
+//  Clocker
+//
+//  Created by lijun on 16/11/7.
+//  Copyright © 2016年 lijun. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface ClockerView : ScreenSaverView
+
+@end
---
+++ Clocker/ClockerView.m
@@ -0,0 +1,52 @@
+//
+//  ClockerView.m
+//  Clocker
+//
+//  Created by lijun on 16/11/7.
+//  Copyright © 2016年 lijun. All rights reserved.
+//
+
+#import "ClockerView.h"
+
+@implementation ClockerView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
